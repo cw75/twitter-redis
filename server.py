c8dc8a59f7074ec12063cf388b1c4b3598c6bc3c
cw75 <cgwu0530@gmail.com>
2020-06-29 17:48:51 +0000

    dummy

--- server.py
+++ server.py
@@ -1,2 +1,11 @@
+import logging
+import sys
+
+logging.basicConfig(filename='log_benchmark.txt', level=logging.INFO,
+                    format='%(asctime)s %(message)s')
+
+logging.info(sys.argv[1])
+logging.info(sys.argv[2])
+
 while True:
-	a = 1
+    a = 1
